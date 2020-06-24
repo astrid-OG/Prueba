c9da3d318ac7afd6ab89898bcad328c4ba3145e7
luisamgomezg <42420401+luisamgomezg@users.noreply.github.com>
2020-06-24 08:44:58 -0500

    agregue carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import { Grid, Cell } from "styled-css-grid";
+import Carousel from './Carousel/Carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Grid
+      columns={"100px 1fr 100px"}
+      rows={"minmax(45px,auto) 1fr minmax(45px,auto)"}>
+      <Cell width={3}>
+        <h1>VIAJA POR EL MUNDO</h1>
+      </Cell>
+
+      <Cell> 
+          Menu
+      </Cell>
+      
+      <Cell><Carousel/></Cell>
+      <Cell>Ads</Cell>
+
+      <Cell width={3}>
+        Footer
+      </Cell>
+      </Grid>
       </header>
     </div>
   );
---
+++ src/Carousel/Carousel.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './Carousel.css';
+import { Carousel } from "react-responsive-carousel";
+
+
+export default () => (
+    <Carousel autoPlay>
+    <div >
+      <img alt="" className="redondeado" src="https://img.freepik.com/foto-gratis/avion-juguete-cerca-suministros-viaje_23-2147746341.jpg?size=626&ext=jpg" />
+      <p className="legend">VIAJES</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="https://img.freepik.com/foto-gratis/silla-mesa-comedor-playa-mar-cielo-azul_74190-6094.jpg?size=626&ext=jpg" />
+      <p className="legend">PLAYA</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="https://img.freepik.com/foto-gratis/palma-recorrido-lujo-bora-caribe_1203-5350.jpg?size=626&ext=jpg" />
+      <p className="legend">MALDIVAS</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="https://image.freepik.com/foto-gratis/panorama-piramides-teotihuacan_163782-417.jpg" />
+      <p className="legend">MEXICO</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="https://img.freepik.com/foto-gratis/bayside-marketplace-al-atardecer-miami-florida_29285-1088.jpg?size=626&ext=jpg" />
+      <p className="legend">MIAMI</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="https://img.freepik.com/foto-gratis/hermosa-panoramica-torre-eiffel-paris-rodeada-agua-barcos-cielo-colorido_181624-5118.jpg?size=626&ext=jpg" />
+      <p className="legend">PARIS</p>
+    </div>
+      
+    </Carousel>
+  );
