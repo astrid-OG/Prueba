ad1719b12e9e7bf86577d8916efa523f2bb6ab2b
luisamgomezg <42420401+luisamgomezg@users.noreply.github.com>
2020-06-24 09:28:26 -0500

    hola

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Carousel.css';
 import { Carousel } from "react-responsive-carousel";
 
-
+const hola='hola'
 export default () => (
     <Carousel autoPlay>
     <div >
