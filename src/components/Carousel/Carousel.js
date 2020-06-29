541dfe0f1448b0b4e00f70ac440147b2475bc781
juanchope94 <mjuan@unicauca.edu.co>
2020-06-29 13:13:20 -0500

    borrando fotota

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -7,10 +7,60 @@ export default () => (
   
     <Carousel autoPlay showThumbs={false}>
     <div >
-      <img alt="" className="redondeado"  src="fotota.jpeg" />
-      <p className="legend">FOTOTA!!</p>
+      <img alt="" className="redondeado"  src="3.jpg" />
+      <p className="legend">CARTAGENA</p>
     </div>
-    
-
+    <div>
+      <img alt="" className="redondeado" src="1.jpg" />
+      <p className="legend">PLAYA BLANCA</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="2.jpg" />
+      <p className="legend">CARTAGENA</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="4.jpg" />
+      <p className="legend">SAN FELIPE DE BARAJAS</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="5.jpg" />
+      <p className="legend">TERMALES MANIZALES</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="6.jpg" />
+      <p className="legend">FAMILIA</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="7.jpg" />
+      <p className="legend">EL BOSQUE PALMIRA</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="8.jpg" />
+      <p className="legend">PARQUE DEL CAFE</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="9.jpg" />
+      <p className="legend">KRATER PARQUE DEL CAFE</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="10.jpg" />
+      <p className="legend">PARQUE DEL CAFE</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="11.jpg" />
+      <p className="legend">SAN FELIPE DE BARAJAS</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="12.jpg" />
+      <p className="legend">EL PEÑOL</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="13.jpg" />
+      <p className="legend">EL PEÑOL</p>
+    </div>
+    <div>
+      <img alt="" className="redondeado" src="14.jpg" />
+      <p className="legend">CARTAGENA</p>
+    </div> 
     </Carousel>
   );
